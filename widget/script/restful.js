15b0714be92da28d63a416cc3c54fb680db5f3d8
sunyongxiang <sunyongxiang@mobi-app.cn>
2019-12-10 14:51:58 +0800

    Fix: restful文件修改

--- widget/script/restful.js
+++ widget/script/restful.js
@@ -14,53 +14,20 @@ var TOKEN_BEOVERDUE = 40001; // token过期
 // var host = "http://192.168.1.28:99";
 
 
-var host = 'http://47.103.28.164:9001';
+var host = 'http://192.168.1.28:9102';
 
 //外网测试地址
 // var host = "http://whale123.free.idcfengye.com";
 var interfaces = {
-    login: '/jgj/auth/login',// 登录
+    login: '/sw/auth/login',// 登录
+    register:'/sw/auth/register', //注册
+    bindPhone:'/sw/auth/bind_phone', //绑定手机号
+    setPwd:'/sw/auth/reset_pwd',    //修改密码
     loginId: '/jgj/worker/worker_type', // 判断用户登录身份
     logout: '/jgj/worker/logout',//退出登录
     sendCode: '/api/common/send_code',//发送验证码
-    mine: '/jgj/worker/info',//我的
-    orderList: '/jgj/order/list',//订单列表
-    log: '/jgj/order/log',//打电话/发短信记录日志
-    returnLog: '/jgj/after_sale/log',//售后打电话/发短信记录日志
-    nextSevenDayOrder: '/jgj/order/next_seven_day',//后七天订单列表
-    afterSaleOrder: '/jgj/after_sale/list',//售后单列表
-    afterSaleOrderDetail: '/jgj/after_sale/detail',//售后单详情
-    startService: '/jgj/order/begin',//开始服务
-    endService: '/jgj/order/complete',//结束服务
-    orderDetail: '/jgj/order/detail',//订单详情
-    supplementPrice: '/jgj/order/raise_price',//补价列表
-    returnPrice: '/jgj/order/refund',//退款
-    schedule: '/jgj/schedule/list',//师傅排期
-    saveLeaveMsg: '/jgj/after_sale/leave_msg',//售后订单保存留言
-    askHelp: '/jgj/after_sale/need_assist',//请求协助
-    startDeal: '/jgj/after_sale',//开始处理
-    endDeal: '/jgj/after_sale/complete',//处理完成
-    savePhoto: '/jgj/order/service_photo',//拍照
-    savePhotoAfterSale: '/jgj/after_sale/photo',//售后拍照
-    uploadImg: '/api/common/upload_image',//上传图片
-    stopOrderList: '/jgj/stop_order/list',//停单申请列表
-    stopOrderApply: '/jgj/stop_order',//提交/编辑停单申请
 
-    salaryFlow: '/jgj/salary_flow',//资金流水
-    walletDetail: '/jgj/salary_flow/list',//钱包明细
-    summarize: '/jgj/salary_flow/summarize',//流水结算汇总
-    sureMoney: '/jgj/salary_flow/confirm',//确认工资
-    deductList: '/jgj/salary_flow/deduct',//扣款列表
-    deduct_num: '/jgj/salary_flow/deduct_num',//扣款总数
-    notice: '/jgj/message/',//消息
-    feedBack: '/jgj/salary_flow/feedBack', //提交意见反馈
-};
-var bmapkey = 'rNXG9T89mOqZXRh0N3M1NgWqQVd6OGuw';
-var RSA_PUBLIC = {
-    modulus: '-----BEGIN PUBLIC KEY-----\
-      MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAIqrTXHHzJdmKhGZyp5RcQ24+j7gGDAYK4/YxiE9XoeCjizksDGLkK/lpqeK+flUOLp/uk0tej1QRKAk3BTGxTMCAwEAAQ==\
-     -----END PUBLIC KEY-----'//系数
-    , exponent: 'AQAB'//指数
+
 };
 
 /**
@@ -87,7 +54,7 @@ function fnGet(apiurl, param, showLoading, callback) {
             headers: {
                 nonceStr: nonceStr,
                 timeStamp: timeStamp,
-                access_token: accessToken
+                access-token: accessToken
             },
             data: {
                 values: param
@@ -133,7 +100,7 @@ function fnDelete(apiurl, showLoading, callback) {
             headers: {
                 nonceStr: nonceStr,
                 timeStamp: timeStamp,
-                access_token: accessToken,
+                access-token: accessToken,
                 "Content-Type": APPLICATION_JSON_UTF8_FORM
             }
         },
@@ -176,7 +143,7 @@ function fnPut(apiurl, data, callback) {
             headers: {
                 nonceStr: nonceStr,
                 timeStamp: timeStamp,
-                access_token: accessToken,
+                access-token: accessToken,
                 "Content-Type": APPLICATION_JSON_UTF8_FORM
             },
             data: {
@@ -220,7 +187,7 @@ function fnBodyPut(apiurl, data, callback) {
             headers: {
                 nonceStr: nonceStr,
                 timeStamp: timeStamp,
-                access_token: accessToken,
+                access-token: accessToken,
                 "Content-Type": APPLICATION_JSON_UTF8_VALUE
             },
             data: {
@@ -265,7 +232,7 @@ function fnPost(apiurl, formdata, callback) {
             headers: {
                 nonceStr: nonceStr,
                 timeStamp: timeStamp,
-                access_token: accessToken,
+                access-token: accessToken,
                 "Content-Type": APPLICATION_JSON_UTF8_FORM
             },
             data: {
@@ -311,7 +278,7 @@ function fnBodyPost(apiurl, formdata, callback) {
             headers: {
                 nonceStr: nonceStr,
                 timeStamp: timeStamp,
-                access_token: accessToken,
+                access-token: accessToken,
                 "Content-Type": APPLICATION_JSON_UTF8_VALUE
             },
             data: {
